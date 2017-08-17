f201f07bae69afb1d93622a4fb4ad12495dff704
yachun0728 <yachun0728@gmail.com>
2017-08-17 10:23:35 +0800

    Production Code and test

---
+++ DeadAnts/DeadAnts/Kata.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DeadAnts
+{
+    public class Kata
+    {
+        public static int DeadAntCount(string ants)
+        {
+            int deadAntCount = 0;
+
+
+            if (string.IsNullOrEmpty(ants))
+                return deadAntCount;
+            if (ants.Substring(0, 1) != "a")
+                deadAntCount++;
+
+            var eachAnts = ants.Split('a');
+
+            foreach (var ant in eachAnts)
+            {
+                deadAntCount = CheckAntCount(ant, deadAntCount);
+            }
+            return deadAntCount - 1;
+        }
+
+        private static int CheckAntCount(string ant, int deadAntCount)
+        {
+            if (ant.Length == 0 || ant.Length < 2)
+            {
+                deadAntCount++;
+            }
+            else
+            {
+                var first = ant.Substring(0, 1);
+                var second = ant.Substring(1, 1);
+                if (first != "n" || first+second != "nt")
+                {
+                    deadAntCount++;
+                }
+            }
+            return deadAntCount;
+        }
+    }
+}
---
+++ DeadAnts/DeadAntsTests/KataTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeadAnts.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void Input_4_ant_no_one_dead()
+        {
+            Assert.AreEqual(0, Kata.DeadAntCount("ant ant ant ant"));
+        }
+        
+        [TestMethod]
+        public void Input_null_no_one_dead()
+        {
+            Assert.AreEqual(0, Kata.DeadAntCount(null));
+        }
+        
+        [TestMethod]
+        public void Input_2_dead_ants_can_get_2_dead_ants()
+        {
+            Assert.AreEqual(2, Kata.DeadAntCount("ant anantt aantnt"));
+        }
+
+        [TestMethod]
+        public void Input_1_dead_ants_can_get_1_dead_ants()
+        {
+            Assert.AreEqual(1, Kata.DeadAntCount("ant ant .... a nt"));
+        }
+
+        [TestMethod]
+        public void Input_10_dead_ant_can_get_10_dead_ants()
+        {
+            Assert.AreEqual(10, Kata.DeadAntCount("nt.aaaa..atn...t..an.tana.t..nt..tnttant.annna.n...t."));
+        }
+        [TestMethod]
+        public void Input_11_dead_ant_can_get_11_dead_ants()
+        {
+            Assert.AreEqual(11, Kata.DeadAntCount("tnn...tnt...nat.t..tn.na.at.naaanat.aaa..atn..ntt.n.."));
+        }
+    }
+}
